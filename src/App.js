9d6d343e96c318d8e3845d3e3da21f228227f732
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-04-12 19:28:00 +0200

    update cra

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React from 'react';
 import './App.css';
 
-const initialProductList = [
-  { id: 1, name: 'produit 1', price: 50, quantity: 1 },
-  { id: 2, name: 'produit 2', price: 75, quantity: 2 },
-  { id: 3, name: 'produit 3', price: 20, quantity: 5 }
-];
-
-function App () {
-  return (
-    <div className='App'>
-      <h1>Ma commande</h1>
-    </div>
-  );
+function App() {
+  return <div className='App'></div>;
 }
 
 export default App;
